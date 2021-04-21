00128fa8f5bac88cd0d0ca8e732c75f9476e585a
guiguicdd1 <50055027+guiguicdd1@users.noreply.github.com>
2021-04-21 11:21:41 -0300

    v0.0.006

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ async function starts() {
 				}
 			}
 			const botNumber = client.user.jid
-			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const isGroup = from.endsWith('@g.us')
+			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
 			const groupMembers = isGroup ? groupMetadata.participants : ''
 			const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
