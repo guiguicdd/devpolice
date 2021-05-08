185c95a29fc0e90be26ed7b26873e0814d23d7e6
guiguicdd1 <50055027+guiguicdd1@users.noreply.github.com>
2021-05-08 15:32:37 -0300

    v4.5.007

--- index.js
+++ index.js
@@ -264,8 +264,7 @@ async function starts() {
 					// if (isDocument) return reply(content + '\n\nDocument')
 					// reply(content)
 					if (isGroup) {
-						if (budy.slice(0).toLowerCase().includes('https://chat.whatsapp.com/') && await client.groupInviteCode(from) != budy.slice(0).toLowerCase().split("https://chat.whatsapp.com/")[1].substr(0, 22)) return await client.groupRemove(from, [sender])
-
+						if (budy.slice(0).toLowerCase().includes('https://chat.whatsapp.com/')) return await client.groupRemove(from, [sender])
 						startuserverification(client, budy, from, mek, sender, palavroes, spamcheker, usersjson, text, sticker, isGroup, isFakenews, umstopoint, reply)
 					} else {
 						pvdevpolice(budy, sender, usersjson, client, reply)
